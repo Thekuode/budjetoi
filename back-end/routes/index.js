f8341512c29a41a3637498f210dec49322ae8e12
Roni <ronisvlnn@gmail.com>
2025-08-05 16:17:25 +0300

    more changes

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function(req, res, next){
+  res.send('Hello World!')
+})
+
+router.post('/',function(req, res, next) {
+  res.send('got a POST request')
+})
 
 module.exports = router;
