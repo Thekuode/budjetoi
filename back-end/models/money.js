0c5139ae32c383e657bc322eacd86f2eea78300e
Roni <ronisvlnn@gmail.com>
2025-08-05 17:57:57 +0300

    changed taht descrpition can be only palkka or lahja

--- back-end/models/money.js
+++ back-end/models/money.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const moneySchema = new mongoose.Schema({
   amount: { type: Number, required: true },
-  description: { type: String, required: true }
+  description: { type: String, required: true, enum: ['palkka', 'lahja'] }
 });
 
 module.exports = mongoose.model('Money', moneySchema);
