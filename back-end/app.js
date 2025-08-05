24893d5f618ac25c6583db941c30de0872d3b840
Roni <ronisvlnn@gmail.com>
2025-08-05 17:45:15 +0300

    changed budjettiapp to mongo uri

--- back-end/app.js
+++ back-end/app.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 
-const mongoUri = 'mongodb+srv://ronisvlnn:24x4nCpCZUSFI21M@cluster0.qsmk4zv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoUri = 'mongodb+srv://ronisvlnn:24x4nCpCZUSFI21M@cluster0.qsmk4zv.mongodb.net/BudjettiApp?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.connect(mongoUri)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
-  
+
 const homeRouter = require('./routes/home')
 const moneyRouter = require('./routes/money')
 
